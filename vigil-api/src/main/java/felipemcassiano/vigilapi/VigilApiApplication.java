89069ad8c221d27b16284313849c48d4b1464579
FelipeMCassiano <felipemcassiano@gmail.com>
2026-01-21 18:44:45 -0300

    init api

---
+++ vigil-api/src/main/java/felipemcassiano/vigilapi/VigilApiApplication.java
@@ -0,0 +1,13 @@
+package felipemcassiano.vigilapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigilApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VigilApiApplication.class, args);
+    }
+
+}
---
+++ vigil-api/src/test/java/felipemcassiano/vigilapi/VigilApiApplicationTests.java
@@ -0,0 +1,13 @@
+package felipemcassiano.vigilapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigilApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
